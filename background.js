602182b4aa103451fb9ab7800496cb15047a3d68
Anand Patel <anandpatel3@hotmail.com>
2018-08-18 11:11:58 -0400

    changes

--- background.js
+++ background.js
@@ -39,7 +39,7 @@ function kissanimeVideoScript(tabId, changeInfo, tab) {
 	} 
 }
 function desitvboxScript(tabId, changeInfo, tab) {
-	if (/http:\/\/business-articles.*/gi.test(tab.url)) {
+	if (/http:\/\/business-articles.*/gi.test(tab.url) || /http:\/\/business-cash.*/gi.test(tab.url)) {
 		chrome.tabs.executeScript(tabId, {file: 'desitvboxScript.js'})
 	} 
 }
@@ -58,7 +58,8 @@ var ctxMenuScripts = {
 }
 
 chrome.runtime.onInstalled.addListener(onInstalled);
-chrome.tabs.onUpdated.addListener(kissanimeVideoScript)
-chrome.tabs.onUpdated.addListener(desitvboxScript)
 chrome.contextMenus.onClicked.addListener(contextMenuOnClickHandler);
 chrome.commands.onCommand.addListener(cmdActivateHandler)
+
+chrome.tabs.onUpdated.addListener(kissanimeVideoScript)
+chrome.tabs.onUpdated.addListener(desitvboxScript)
--- desitvboxScript.js
+++ desitvboxScript.js
@@ -1,17 +1,7 @@
-// var quality = document.getElementById('slcQualix')
-
-// // select the highest quality
-// quality.childNodes[1].selected = true;
-// // this doesnt work for somereason b/c video src doesnt reload, probably has to do something with the bubbling feature
-// // you have to manually click the quality but if logged in, select default as highest quality to load it
-
-// vidUrl = document.getElementById('my_video_1_html5_api').src
-
-// window.location = vidUrl
 console.log('desitvbosScript.js LAUNCHED')
 var iframes = document.querySelectorAll('iframe')
 iframes.forEach(iframe => {
-    if(iframe.src.indexOf('tvlogy') >= 0) {
+    if(iframe.src.indexOf('tvlogy') >= 0 || iframe.src.indexOf('dailymotion') >= 0) {
         window.location = iframe.src
     }
 });
